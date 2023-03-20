7bb785a161f1ecf76d56c2d6365f20c5db062b8c
Pedro Gutierrez Jr <49994182+PedroG1018@users.noreply.github.com>
2023-03-20 10:27:28 -0700

    Add files via upload

---
+++ cocktails/src/pages/About.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <section className='section about-section'>
+      <h1 className='section-title'>about us</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit similique autem quaerat cum placeat illo aliquam magni tempore fuga minus. Possimus laboriosam quis illum mollitia quisquam fugit dolor nisi aliquam atque consectetur. Dicta illo dolorem labore recusandae saepe delectus, porro deserunt laborum consequuntur ut.
+      </p>
+    </section>
+  )
+}
+
+export default About
